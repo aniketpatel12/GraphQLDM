c2c3ea72610f47eded14a7c1fedb00e9ce9bd016
ANIKET PATEL <62117194+aniketpatel12@users.noreply.github.com>
2024-04-28 08:28:12 +0000

    Added setup file for an easy installtion!

---
+++ setup.py
@@ -0,0 +1,17 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='graphqldm',
+    version='0.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'graphqldm = graphqldm.cli:main'
+        ]
+    },
+    install_requires=[
+        'requests',
+    ],
+)
