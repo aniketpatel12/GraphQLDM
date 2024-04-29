a6bab98c31216a71962d52f78fec0a008356c28f
ANIKET PATEL <62117194+aniketpatel12@users.noreply.github.com>
2024-04-29 04:16:50 +0000

    Change the name of the shortend name of the tool

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'graphqldm = graphqldm.cli:main'
+            'gqldm = graphqldm.cli:main'
         ]
     },
     install_requires=[
